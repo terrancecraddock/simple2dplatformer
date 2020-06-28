0c0960cfd44423086becda5d0d2a96a807935cfb
terrancecraddock <terrancecraddock@gmail.com>
2020-06-27 23:52:27 -0400

    Add files via upload

---
+++ cameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    private Vector3 offset;
+    //public Camera camera;
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+    private void Update()
+    {
+
+    }
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
